9ff0bf99e355d4733a3a133eb17d4e2dec207853
Robin Montérémal <robin.monteremal@gmail.com>
2020-01-14 17:33:30 +0100

    Ajout support prediction pour algos Machine Learning

--- webservices/prediction/Prediction.py
+++ webservices/prediction/Prediction.py
@@ -15,38 +15,58 @@ device = torch.device("cuda:0")
 
 class Prediction():
 
-    def __init__(self, dir_from, algo, dir_img, nomImage):
-        logging.info('prediction.init')
-        self.directory_from = dir_from
-        self.algo = algo
-        if self.algo == 'cnn':
-            self.file = 'Medium_CNN_trained.pt'
-        else:
-            self.file = self.algo + "_trained.pt"
-        self.dir_img = dir_img
-        self.nomImage = nomImage
+	def __init__(self, dir_from, algo, dir_img):
+		logging.info('prediction.init')
+		self.directory_from = dir_from
+		self.algo = algo
+		self.dir_img = dir_img
+		if self.algo == 'cnn':
+			self.file = 'Medium_CNN_trained.pt'
+		else:
+			self.file = self.algo + "_trained.pt"
+		self.image = self.read_image(self.dir_img, 240)
 
-    def run(self):
-        if self.algo == 'cnn':
-            model = brainCNN()
-        elif self.algo == 'resnet':
-            model = models.resnet18()
-            num_ftrs = model.fc.in_features
-            model.fc = nn.Linear(num_ftrs, 2)
-        elif self.algo == 'alexnet':
-            model = models.alexnet()
-            model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 2)
-        elif self.algo == 'vgg':
-            model = models.vgg16()
-            num_features = model.classifier[6].in_features
-            features = list(model.classifier.children())[:-1]
-            features.extend([nn.Linear(num_features, 2)])
-            model.classifier = nn.Sequential(*features)
-        model = model.to(device)
-        logging.info('prediction.loadModel')
-        model.load_state_dict(torch.load(self.directory_from+self.file))
-        img = Image.open(self.dir_img + self.nomImage)
-        logging.info('prediction.testModel')
-        label, proba = test_model(model, np.asarray(img))
-        logging.info('Label found : ' + str(label) + ' - Probability : ' + str(proba))
-        return label, proba
+	def read_image(self, path, img_size = 0):
+		logging.info('prediction_ML.read_image')
+		img = 0
+	
+		try:
+			img = Image.open(path)
+			if img_size != 0:
+				img = img.resize((img_size, img_size))
+			img = img.convert('L').convert('RGB')
+			img = np.asarray(img).flatten()
+				
+		except IOError as err:
+			logging.error("Error reading image or path")
+			logging.error(err)
+
+		except Exception as err:
+			logging.error("Unkownown error in read_image")
+			logging.error(err)
+
+		return img
+
+	def run(self):
+		if self.algo == 'cnn':
+			model = brainCNN()
+		elif self.algo == 'resnet':
+			model = models.resnet18()
+			num_ftrs = model.fc.in_features
+			model.fc = nn.Linear(num_ftrs, 2)
+		elif self.algo == 'alexnet':
+			model = models.alexnet()
+			model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 2)
+		elif self.algo == 'vgg':
+			model = models.vgg16()
+			num_features = model.classifier[6].in_features
+			features = list(model.classifier.children())[:-1]
+			features.extend([nn.Linear(num_features, 2)])
+			model.classifier = nn.Sequential(*features)
+		model = model.to(device)
+		logging.info('prediction.loadModel')
+		model.load_state_dict(torch.load(self.directory_from+self.file))
+		logging.info('prediction.testModel')
+		label, proba = test_model(model, self.image)
+		logging.info('Label found : ' + str(label) + ' - Probability : ' + str(proba))
+		return label, proba
--- webservices/prediction/service_prediction.py
+++ webservices/prediction/service_prediction.py
@@ -3,52 +3,69 @@ from flask import request
 import os
 
 from Prediction import Prediction
+from Prediction_ML import Prediction_ML
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-list_algo = ["cnn", "resnet", "alexnet", "vgg"]
-
+# 4 algos deep learning
+# 5 algos sklearn
+list_algo_deep = ["cnn", "resnet", "alexnet", "vgg"]
+list_algo_ml = ["knn", "svm", "gbc", "rfc", "nn"]
 
 @app.route('/api/v1/prediction', methods=['GET'])
 def prediction():
+	if request.args.get('directory_from') is None:
+		return 'No "directory_from" given.'
+	else:
+		directory_from = request.args.get('directory_from')
 
-    if request.args.get('directory_from') is None:
-        return 'No "directory_from" given.'
-    else:
-        directory_from = request.args.get('directory_from')
+	if not os.path.exists(directory_from):
+		return '"directory_from" cannot be found.'
 
-    if not os.path.exists(directory_from):
-        return '"directory_from" cannot be found.'
+	if request.args.get('algo') is None:
+		return 'No "algo" given.'
+	elif (not(request.args.get('algo') in list_algo_deep) and not(request.args.get('algo') in list_algo_ml)):
+		return 'Incorrect "algo" provided.'
+	else:
+		algo = request.args.get('algo')
 
-    if request.args.get('algo') is None:
-        return 'No "algo" given.'
-    elif not(request.args.get('algo') in list_algo):
-        return 'Incorrect "algo" provided.'
-    else:
-        algo = request.args.get('algo')
+	if request.args.get('directory_img') is None:
+		return 'No "directory_img" given.'
+	else:
+		directory_img = request.args.get('directory_img')
 
-    if request.args.get('directory_img') is None:
-        return 'No "directory_img" given.'
-    else:
-        directory_img = request.args.get('directory_img')
+	if not os.path.exists(directory_img):
+		return '"directory_img" cannot be found.'
 
-    if not os.path.exists(directory_img):
-        return '"directory_img" cannot be found.'
+	# if request.args.get('name_img') is None:
+		# return 'No "name_img" given.'
+	# else:
+		# name_img = request.args.get('name_img')
 
-    if request.args.get('name_img') is None:
-        return 'No "name_img" given.'
-    else:
-        name_img = request.args.get('name_img')
+	# if not os.path.exists(directory_img + name_img):
+		# return 'img ' + name_img + ' cannot be found.'
 
-    if not os.path.exists(directory_img + name_img):
-        return 'img ' + name_img + ' cannot be found.'
+	# type algo choisi
+	if algo in list_algo_deep:
+		pred = Prediction(directory_from, algo, directory_img)
+		label, proba = pred.run()
 
-    pred = Prediction(directory_from, algo, directory_img, name_img)
+		if label == -1:
+			return 'Error model '+str(algo)+' is not trained yet!\nTrain this model first before using it for predictions'
+		else:
+			return 'Prediction using ' + str(algo) + ' done, Label found : ' + str(label) + ' - Probability : ' + str(proba)
 
-    label, proba = pred.run()
+	elif algo in list_algo_ml:
+		pred = Prediction_ML(directory_from, algo, directory_img)
+		label = pred.run()
+		
+		if label == -1:
+			return 'Error model '+str(algo)+' is not trained yet!\nTrain this model first before using it for predictions'
+		else:
+			return 'Prediction using ' + str(algo) + ' done, Label found : ' + str(label)
 
-    return 'Prediction done, Label found : ' + str(label) + ' - Probability : ' + str(proba)
-
+	else:
+		return 'Unexpected error, ' + str(algo) + ' is not part of the supported algorithms!'
 
 app.run(port=5007)
