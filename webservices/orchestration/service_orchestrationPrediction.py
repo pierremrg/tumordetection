83b4d9d5e61d0c79947c9287a6c28efc5c5696f9
Theou <theo.grange@numericable.fr>
2020-01-19 01:03:22 +0100

    orchestration prediction

--- webservices/orchestration/service_orchestrationPrediction.py
+++ webservices/orchestration/service_orchestrationPrediction.py
@@ -3,7 +3,7 @@ from flask import request
 import os
 import json
 
-from Orchestration import Orchestration
+from OrchestrationPrediction import OrchestrationPrediction
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,8 +12,8 @@ list_algo_deep = ["cnn", "resnet", "alexnet", "vgg"]
 list_algo_ml = ["knn", "svm", "gbc", "rfc", "nn"]
 
 # Pour lancer ce service en local, il faut avoir modifié les ports des microservices (par ex, app.run(port = 5001)) car sinon impossible de lancer plusieurs apps flask en même temps
-@app.route('/api/v1/orchestrationTraining', methods=['GET'])
-def orchestrationTraining():
+@app.route('/api/v1/orchestrationPrediction', methods=['GET'])
+def orchestrationPrediction():
 
     #JSON (url_db & classifiers)
     if request.args.get('json') is None:
@@ -22,7 +22,7 @@ def orchestrationTraining():
         data = request.args.get('json')
 
     data = json.loads(data)
-    url = data["url_db"]
+    url_img = data["url_img"]
     classifiers = data["classifiers"]
 
     list_algo = []
@@ -32,17 +32,17 @@ def orchestrationTraining():
             return algo + ' is an incorrect algo.'
         list_algo.append(algo)
 
-    orch = Orchestration(url, list_algo)
-    list_returns_trains = orch.run()
-    string_result = '{ \"returns_trains\": {'
-    for i in range(len(list_returns_trains)):
-        string_result += list_returns_trains[i]
-        if i == len(list_returns_trains) - 1:
+    orchPred = OrchestrationPrediction(url_img, list_algo)
+    list_returns_predict = orchPred.run()
+    string_result = '{ \"returns_predictions\": {'
+    for i in range(len(list_returns_predict)):
+        string_result += list_returns_predict[i]
+        if i == len(list_returns_predict) - 1:
             string_result += '}}'
         else:
             string_result += ','
     return json.loads(string_result)
 
-app.run(host="0.0.0.0", port = 5009)
+app.run(host="0.0.0.0", port = 5013)
 
-#http://localhost:5006/api/v1/orchestrationTraining?json={%22url_db%22:%22aaaa%22,%20%22classifiers%22:[%22resnet%22},%22knn%22]}
+#http://localhost:5013/api/v1/orchestrationPrediction?json={%22url_img%22:%22/home/theo/Bureau/INSA/Integrateur/new_data_norm/yes/Y1.jpg%22,%20%22classifiers%22:[%22resnet%22,%20%22alexnet%22]}
